12eac4424c7439bc3c2e1c56685ddf1fd560504f
PalanikannanM <palanikannan.m2020@vitstudent.ac.in>
2023-03-01 11:49:32 +0000

    [cms] strapi support added for timeline component

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,16 @@
-declare function fetchTimelineData({ endpoint, transferType, }: {
-    endpoint: string;
-    transferType: (data: any) => TimelineObjectType[];
-}): Promise<TimelineObjectType[]>;
+/**
+ * Get full Strapi URL from path
+ * @param {string} path Path of the URL
+ * @returns {string} Full Strapi URL
+ */
+declare function getStrapiURL(path?: string): string;
+/**
+ * Helper to make GET requests to Strapi API endpoints
+ * @param {Object} urlParamsObject URL params object, will be stringified
+ * @param {Object} options Options passed to fetch
+ * @returns Parsed API call response
+ */
+declare function fetchTimelineData(urlParamsObject?: object, options?: object): Promise<TimelineObjectType[]>;
 type TimelineObjectType = {
     id: any;
     title: string;
@@ -15,4 +24,4 @@ declare const Timeline: ({ type, data, }: {
     data: TimelineObjectType[];
 }) => JSX.Element | null;
 
-export { Timeline, TimelineObjectType, fetchTimelineData };
+export { Timeline, TimelineObjectType, fetchTimelineData, getStrapiURL };
--- src/core/timelineAPI.ts
+++ src/core/timelineAPI.ts
@@ -1,14 +1,64 @@
-export async function fetchTimelineData({
-  endpoint,
-  transferType,
-}: {
-  endpoint: string;
-  transferType: (data: any) => TimelineObjectType[];
-}) {
-  const res = await fetch(endpoint);
-  const data = await res.json();
-  const timelineResults = transferType(data);
-  return timelineResults;
+import qs from "qs";
+
+/**
+ * Get full Strapi URL from path
+ * @param {string} path Path of the URL
+ * @returns {string} Full Strapi URL
+ */
+export function getStrapiURL(path: string = ""): string {
+  return `${
+    process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337"
+  }${path}`;
+}
+
+/**
+ * Helper to make GET requests to Strapi API endpoints
+ * @param {Object} urlParamsObject URL params object, will be stringified
+ * @param {Object} options Options passed to fetch
+ * @returns Parsed API call response
+ */
+export async function fetchTimelineData(
+  urlParamsObject: object = {},
+  options: object = {}
+): Promise<TimelineObjectType[]> {
+  const mergedOptions = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    ...options,
+  };
+
+  const queryString = qs.stringify(urlParamsObject);
+  const requestUrl = `${getStrapiURL(
+    `/api/timelines${queryString ? `?${queryString}` : ""}`
+  )}`;
+
+  const response = await fetch(requestUrl, mergedOptions);
+
+  if (!response.ok) {
+    console.error(response.statusText);
+    throw new Error(`An error occured please try again`);
+  }
+  const data = await response.json();
+  return transferType(data);
+}
+
+/**
+ * Helper to make GET requests to Strapi API endpoints
+ * @param {Object} data Data from Strapi
+ * @returns {TimelineObjectType[]} Array of Timeline objects
+ */
+function transferType(data: any): TimelineObjectType[] {
+  const timeline = data.data;
+  return timeline.map((timelineData: any) => {
+    return {
+      id: String(timelineData.id),
+      title: String(timelineData.attributes.title),
+      meta: String(timelineData.attributes.meta),
+      subtitle: String(timelineData.attributes.subtitle),
+      description: String(timelineData.attributes.description),
+    };
+  });
 }
 
 export type TimelineObjectType = {
